455c1a02ebc0f6bff3a3fb1627d0a015e9c13843
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-02-08 23:02:28 +0530

    Added more details to markers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Visualizing Landmarks
 Visualization for Volcanoes! (More details to follow!)
+
+![images/Volcanoes](images/Volcanoes.png)
--- map.py
+++ map.py
@@ -2,13 +2,31 @@
 import folium
 # Import pandas : https://pandas.pydata.org/pandas-docs/stable/index.html
 import pandas as pd
+# from ipywidgets import interact
+
 
 # print(dir(folium))
 
 data = pd.read_csv('Dataset.csv').fillna(value = 0)
-# [819 rows x 36 columns]
+print(data.head())
 
 location = data['Latitude'].mean(), data['Longitude'].mean()
+
+# Cloudmade Mapbox needs an API key, Mapbox Control Room is limited to a few levels
+# tiles = [name.strip() for name in """
+#     OpenStreetMap
+#     Mapbox Bright
+#     Mapbox Control Room
+#     Stamen Terrain
+#     Stamen Toner
+#     Stamen Watercolor
+#     CartoDB positron
+#     CartoDB dark_matter""".strip().split('\n')]
+
+# @interact(lat=(-90., 90.), lon=(-180., 180.), tiles=tiles, zoom=(1, 18))
+# def create_map(lat=52.518611, lon=13.408333, tiles="Stamen Toner", zoom=10):
+#     return folium.Map(location=(lat, lon), tiles=tiles, zoom_start=zoom)
+
 m = folium.Map(location=location, zoom_start=3, tiles='Stamen Terrain')
 # m.add_child(folium.LatLngPopup())
 
@@ -25,10 +43,17 @@ def col(ele):
         color = 'red'
     return color
 
+def Vei(num):
+    if num == 0:
+        vei = 'Not available'
+    else:
+        vei = str(num)
+    return vei
 
-for name, lat, long, elev, vei in zip(data['Name'], data['Latitude'], data['Longitude'], data['Elevation'], data['VEI']):
-    popup = folium.Popup("This is the " + name + " volcano", parse_html=True)
-    folium.Marker(location=[lat, long], popup=popup, icon=folium.Icon(color=col(elev))).add_to(m)
+for year, name, latitude, longitude, elev, vei, vol_type, status in zip(data['Year'], data['Name'], data['Latitude'], data['Longitude'], data['Elevation'], data['VEI'], data['Type'], data['Status']):
+    popup = folium.Popup("This is the " + name + " volcano which erupted in " + str(year) + ". It is a " + vol_type + " with " + status + " status and an elevation of " + str(elev) + 
+                            "m. The Volcano Explosivity Index of the eruption was " + Vei(vei) + ".", parse_html=True)
+    folium.Marker(location=[latitude, longitude], popup=popup, icon=folium.Icon(color=col(elev))).add_to(m)
 
 #    if vei != 0:
 #        vei_str = "VEI = " + str(vei)
@@ -41,20 +66,3 @@ for name, lat, long, elev, vei in zip(data['Name'], data['Latitude'], data['Long
    # folium.Marker(location=[lat, long]).add_to(m)
 
 print(m.save('map.html'))
-
-# from ipywidgets import interact
-
-# # Cloudmade Mapbox needs an API key, Mapbox Control Room is limited to a few levels
-# tiles = [name.strip() for name in """
-#     OpenStreetMap
-#     Mapbox Bright
-#     Mapbox Control Room
-#     Stamen Terrain
-#     Stamen Toner
-#     Stamen Watercolor
-#     CartoDB positron
-#     CartoDB dark_matter""".strip().split('\n')]
-
-# @interact(lat=(-90., 90.), lon=(-180., 180.), tiles=tiles, zoom=(1, 18))
-# def create_map(lat=52.518611, lon=13.408333, tiles="Stamen Toner", zoom=10):
-#     return folium.Map(location=(lat, lon), tiles=tiles, zoom_start=zoom)
