5f31a4d85d7a42729c54b41dce3bdf4678e394c8
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-02-14 14:40:28 +0530

    Added Flask Structure

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=('GET','POST'))
+def home():
+    if request.method == 'GET':
+        return render_template("homepage.html")
+
+if __name__ == "__main__":
+	app.run(debug = True)
