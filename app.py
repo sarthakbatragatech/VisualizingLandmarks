ddc0489c04abd435f131c4ae601145128977e4ac
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-02-15 14:37:33 +0530

    Added cache busting for static files

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+from map import create_map
+
 
 app = Flask(__name__)
+
+# Snippet for cache busting CSS files
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
 @app.route("/", methods=('GET','POST'))
 def home():
     if request.method == 'GET':
+        create_map()
         return render_template("homepage.html")
 
 if __name__ == "__main__":
---
+++ map.py
@@ -0,0 +1,68 @@
+# Import folium : https://github.com/python-visualization/folium
+import folium
+# Import pandas : https://pandas.pydata.org/pandas-docs/stable/index.html
+import pandas as pd
+import os
+
+def create_map():
+    # print(dir(folium))
+
+    data = pd.read_csv('Dataset.csv').fillna(value = 0)
+    print(data.head())
+
+    location = data['Latitude'].mean(), data['Longitude'].mean()
+
+    # Cloudmade Mapbox needs an API key, Mapbox Control Room is limited to a few levels
+    # tiles = [name.strip() for name in """
+    #     OpenStreetMap
+    #     Mapbox Bright
+    #     Mapbox Control Room
+    #     Stamen Terrain
+    #     Stamen Toner
+    #     Stamen Watercolor
+    #     CartoDB positron
+    #     CartoDB dark_matter""".strip().split('\n')]
+
+    # @interact(lat=(-90., 90.), lon=(-180., 180.), tiles=tiles, zoom=(1, 18))
+    # def create_map(lat=52.518611, lon=13.408333, tiles="Stamen Toner", zoom=10):
+    #     return folium.Map(location=(lat, lon), tiles=tiles, zoom_start=zoom)
+
+    m = folium.Map(location=location, zoom_start=2, tiles='Stamen Terrain')
+    # m.add_child(folium.LatLngPopup())
+
+
+    # Get Marker color based on elevation
+    def col(ele):
+        if 0 < ele < 1500:
+            color = 'green'
+        elif 1501 < ele < 3000:
+            color = 'yellow'
+        elif 3001 < ele < 5000:
+            color = 'orange'
+        else:
+            color = 'red'
+        return color
+
+    def Vei(num):
+        if num == 0:
+            vei = 'Not available'
+        else:
+            vei = str(num)
+        return vei
+
+    for year, name, latitude, longitude, elev, vei, vol_type, status in zip(data['Year'], data['Name'], data['Latitude'], data['Longitude'], data['Elevation'], data['VEI'], data['Type'], data['Status']):
+        popup = folium.Popup("This is the " + name + " volcano which erupted in " + str(year) + ". It is a " + vol_type + " with " + status + " status and an elevation of " + str(elev) + 
+                                "m. The Volcano Explosivity Index of the eruption was " + Vei(vei) + ".", parse_html=True)
+        folium.Marker(location=[latitude, longitude], popup=popup, icon=folium.Icon(color=col(elev))).add_to(m)
+
+    #    if vei != 0:
+    #        vei_str = "VEI = " + str(vei)
+
+
+    # folium.CircleMarker(location=[lat, long], popup=vei_str, color=col(elev), radius=(int(vei)*10)).add_to(m)
+
+
+    # for lat, long in zip(data['Latitude'], data['Longitude']):
+    # folium.Marker(location=[lat, long]).add_to(m)
+
+    print(m.save('templates/map.html'))
